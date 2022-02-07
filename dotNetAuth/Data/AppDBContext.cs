d9ec63c3031dfba24c920f43a05da90aea015b3e
Conor Canton <conorstreete@gmail.com>
2022-02-07 18:35:53 +0000

    inital setup, created entity models, migrations and migrated to mysql db

---
+++ dotNetAuth/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using dotNetAuth.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetAuth.Data
+{
+    public class AppDBContext : IdentityDbContext<AppUser, IdentityRole, string>
+    {
+        public AppDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}

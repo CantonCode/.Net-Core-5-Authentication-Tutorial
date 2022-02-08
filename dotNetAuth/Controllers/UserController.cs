f55e75bab12e38f08738a8c4e7def40c3eb9e1c1
Conor Canton <conorstreete@gmail.com>
2022-02-08 11:08:47 +0000

    created controller to register new user and add them to db, tested with postman

---
+++ dotNetAuth/Controllers/UserController.cs
@@ -0,0 +1,67 @@
+﻿using dotNetAuth.BindingModel;
+using dotNetAuth.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetAuth.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+       
+
+        private readonly ILogger<UserController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signManager;
+
+
+        public UserController(ILogger<UserController> logger,UserManager<AppUser> userManger, SignInManager<AppUser> signManager)
+        {
+            _logger = logger;
+            _userManager = userManger;
+            _signManager = signManager;
+
+        }
+
+
+        [HttpPost("RegisterUser")]
+        public async Task<object> RegisterUser([FromBody] AddUpdateRegisterUserBindingModel model)
+        {
+            try
+            {
+                var user = new AppUser()
+                {
+                    FullName = model.FullName,
+                    Email = model.Email,
+                    UserName = model.Email,
+                    DateCreated = DateTime.UtcNow,
+                    DateModified = DateTime.UtcNow,
+                };
+
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    return await Task.FromResult("User has been Registered");
+                }
+
+                return await Task.FromResult(result.Errors);
+
+            }
+            catch(Exception ex)
+            {
+                return await Task.FromResult(ex);
+            }
+           
+
+        }
+
+     
+    }
+}
